4e6929d4b3c1f116df018b8e7f8fe05a65e66874
datnguyen79198 <datnguyen79198@gmail.com>
2021-02-17 18:20:16 +0300

    navbar css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { useState }  from 'react';
+import { Navbar, NavbarBrand, NavbarToggler,Collapse,Nav,NavItem } from 'reactstrap';
 
 const Header = () => {
+
+    const [isNavToggle,setisNavToggle] = useState(false);
+    const NavToggle = () => setisNavToggle(!isNavToggle);
+
     return (
-        <>
-            <Navbar className='navbar-color' dark expand="sm">
+        <>  
+            <Navbar className="navbar-color" expand="md">
+
                 <NavbarBrand>
-                    <img src = 'images/logo.png' height="50" width='50'></img>
+                    <img src = 'images/logo.png' height="45" width='45'></img>
                 </NavbarBrand>
+
+                <Nav className="mr-auto" navbar>
+                    <NavItem className='navItem-color thick'> Movies </NavItem>
+                    <NavItem className='navItem-color thick'> TV Shows </NavItem>
+                    <NavItem className='navItem-color thick'> New & Popular </NavItem>
+                    <NavItem className='navItem-color thick'> Watchlist </NavItem>
+                </Nav>
+
             </Navbar>
+
         </>
     )
 }
