44e5078d9ecb2a25ed155602ddccb5c61343f644
datnguyen79198 <datnguyen79198@gmail.com>
2021-02-18 22:00:53 +0300

    basic search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,92 +36,95 @@ const Header = () => {
 
     return (
         <>  
-            <Modal isOpen={signinPopup} size="sm" toggle={popupSigninModal} 
-                   className="rounded modal-custom">
-                <ModalBody> 
-                    <Form>
-                        <FormGroup className='mb-2'>
-                            <Label className="thick-label-text">Sign-In</Label>
-                        </FormGroup>
-                        <FormGroup className='my-2'>
-                            <Label className="label-text" htmlFor='username'>Username</Label>
-                            <Input type="text" />
-                        </FormGroup>
-                        <FormGroup className='my-2'>
-                            <Label className="label-text" htmlFor='password'>Password</Label>
-                            <Input type="password" />
-                        </FormGroup>
-                        <FormGroup className='mt-3 mb-2'>
-                            <Button className="signin-button" disabled={signinLoading} 
-                                    onClick={!signinLoading ? signinClicked : null} block>
-                                        {signinLoading ? 'Loading...' : 'Sign-in'}
-                            </Button>
-                        </FormGroup>
-                        <FormGroup check>
-                            <Label check>
-                                <Input type="checkbox"></Input>
-                                Keep me signed in
-                            </Label>
-                        </FormGroup>
-                        <FormGroup className="mt-1 mb-1" >
-                            <span className="line-custom">
-                                <h2><span>New to server?</span></h2>
-                            </span>
-                            <Button className="mt-2 signup-button" onClick={popupSignupModal} block>
-                                    Create your new account
-                            </Button>
-                        </FormGroup>
-                    </Form>
-                </ModalBody>
-            </Modal>
-
-            <Modal isOpen={signupPop} size="sm" toggle={popupSignupModal}
-                   className="rounded modal-custom" >
-                <ModalBody> 
-                    <Form>
-                        <FormGroup className='mb-2'>
-                            <Label className="thick-label-text">Create account</Label>
-                        </FormGroup>
-                        <FormGroup className='mb-2'>
-                            <Label className="label-text" htmlFor='username'>Your name</Label>
-                            <Input type="text" />
-                        </FormGroup>
-                        <FormGroup className='my-2'>
-                            <Label className="label-text" htmlFor='email'>Email</Label>
-                            <Input type="text" />
-                        </FormGroup>
-                        <FormGroup className='my-2'>
-                            <Label className="label-text" htmlFor='password'>Password</Label>
-                            <Input type="password" />
-                        </FormGroup>
-                        <FormGroup className='my-2'>
-                            <Label className="label-text" >Re-enter Password</Label>
-                            <Input type="password" />
-                        </FormGroup>
-                        <FormGroup className='mt-3 mb-2'>
-                            <Button className="signin-button" block>
-                                Create your account
-                            </Button>
-                        </FormGroup>
-                    </Form>
-                </ModalBody>
-
-                <ModalFooter className="modal-footer-custom">
-                    <Form className="form-footer">
-                        <FormGroup>
-                            <Label>Already has an account? </Label>
-                            <a href="#" onClick={popupSigninModal} className="normal-link-text">  Sign-in</a>
-                        </FormGroup>
-                    </Form>
-                </ModalFooter>
-            </Modal>
-
             <Navbar className="navbar-color bg-transparent" expand="md">
-            
+                {/* sign in modal */}
+                <Modal isOpen={signinPopup} size="sm" toggle={popupSigninModal} 
+                       className="rounded modal-custom">
+                    <ModalBody> 
+                        <Form>
+                            <FormGroup className='mb-2'>
+                                <Label className="thick-label-text">Sign-In</Label>
+                            </FormGroup>
+                            <FormGroup className='my-2'>
+                                <Label className="label-text" htmlFor='username'>Username</Label>
+                                <Input className="signing-input" type="text" />
+                            </FormGroup>
+                            <FormGroup className='my-2'>
+                                <Label className="label-text" htmlFor='password'>Password</Label>
+                                <Input className="signing-input" type="password" />
+                            </FormGroup>
+                            <FormGroup className='mt-3 mb-2'>
+                                <Button className="signin-button" disabled={signinLoading} 
+                                        onClick={!signinLoading ? signinClicked : null} block>
+                                            {signinLoading ? 'Loading...' : 'Sign-in'}
+                                </Button>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox"></Input>
+                                    Keep me signed in
+                                </Label>
+                            </FormGroup>
+                            <FormGroup className="mt-1 mb-1" >
+                                <span className="line-custom">
+                                    <h2><span>New to server?</span></h2>
+                                </span>
+                                <Button className="mt-2 signup-button" onClick={popupSignupModal} block>
+                                        Create your new account
+                                </Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+
+                {/* sign up modal */}
+                <Modal isOpen={signupPop} size="sm" toggle={popupSignupModal}
+                   className="rounded modal-custom" >
+                    <ModalBody> 
+                        <Form>
+                            <FormGroup className='mb-2'>
+                                <Label className="thick-label-text">Create account</Label>
+                            </FormGroup>
+                            <FormGroup className='mb-2'>
+                                <Label className="label-text" htmlFor='username'>Your name</Label>
+                                <Input className="signing-input" type="text" />
+                            </FormGroup>
+                            <FormGroup className='my-2'>
+                                <Label className="label-text" htmlFor='email'>Email</Label>
+                                <Input className="signing-input" type="text" />
+                            </FormGroup>
+                            <FormGroup className='my-2'>
+                                <Label className="label-text" htmlFor='password'>Password</Label>
+                                <Input className="signing-input" type="password" />
+                            </FormGroup>
+                            <FormGroup className='my-2'>
+                                <Label className="label-text" >Re-enter Password</Label>
+                                <Input className="signing-input" type="password" />
+                            </FormGroup>
+                            <FormGroup className='mt-3 mb-2'>
+                                <Button className="signin-button" block>
+                                    Create your account
+                                </Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+
+                    <ModalFooter className="modal-footer-custom">
+                        <Form className="form-footer">
+                            <FormGroup>
+                                <Label>Already has an account? </Label>
+                                <a href="#" onClick={popupSigninModal} className="normal-link-text">  Sign-in</a>
+                            </FormGroup>
+                        </Form>
+                    </ModalFooter>
+                </Modal> 
+
+                {/* Brand */}
                 <NavbarBrand>
                     <img src = 'images/transparent_logo.png' height="50" width='50'></img>
                 </NavbarBrand>
 
+                {/* Nav content */}
                 <Nav navbar className="w-100">
                     <NavItem className="rounded"> 
                         <NavLink href="#" className='px-3 py-1 thick'> Movies  </NavLink> 
@@ -138,7 +141,11 @@ const Header = () => {
                 </Nav>
 
                 <Nav navbar className="w-100 justify-content-end">
-                    {/*<NavItem className='px-3 py-1 rounded navItem-color thick'> Watchlist </NavItem>*/}
+                    <NavItem>
+                        <Input type="text" className="search-bar" placeholder="&#xf002; Title, celeb, genre"
+                                style={{height:"45px", width:"300px"}}/>
+                    </NavItem>
+                    
                     <NavItem className="rounded" onClick = {popupSigninModal}> 
                         <NavLink href="#" className='px-3 py-1 thick'> Sign in </NavLink>
                     </NavItem>
