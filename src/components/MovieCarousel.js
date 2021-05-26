8d22122955f06dc099c6dde364ae9a9c7bfd27fd
datnguyen79198 <datnguyen79198@gmail.com>
2021-05-26 16:23:56 +0300

    update carousel control

--- src/components/MovieCarousel.js
+++ src/components/MovieCarousel.js
@@ -1,27 +1,62 @@
 import React, {useState, useEffect} from 'react'; 
-import { Card, CardImg, CardTitle, Row, Col,
-         Carousel, CarouselControl, CarouselIndicators, CarouselItem } from 'reactstrap';
+import { Card, CardImg, CardLink, Row, Col,
+         Carousel, CarouselControl, CarouselIndicators, CarouselItem, CardBody } from 'reactstrap';
 import AbortController from 'abort-controller';
+
+import { CONFIGURES } from '../config';
 
 const numMovieEachPage = 7;
 const controller = new AbortController();
 const signal = controller.signal;
 
+var poster_controller = new AbortController();
+const poster_signal = poster_controller.signal;
+
 const MovieCarousel = () => {
 
     var [moviesList,setMoviesList] = useState(); 
-
-    const fetchData = (moviesList) => {
-        if (moviesList) {
-            controller.abort();
-        }
-    }
-
-    useEffect(() => {
-        fetchData(moviesList);
-    }, [moviesList]);
+    var [moviePosters, setMoviePosters] = useState([]);
 
     var request_path = '/homepage/getMovieList?2020'; 
+
+    const fetchPosterData = async (movieSet) => {
+        if (movieSet) {
+
+            var posterLinks = [];
+
+            movieSet.map(async (movie) => {
+                let movie_name = movie["primaryTitle"];
+                var requestUrl = "/homepage/search?api_key="+CONFIGURES.IMDB_API_KEY+"&query="+movie_name+/*"&year=" + movie_year+*/"&page=1";
+
+                //console.log(requestUrl);
+
+                poster_controller = new AbortController();
+
+                await fetch(requestUrl, {
+                    method : 'GET',
+                    signal : poster_signal
+                }).then(res=> {
+                    if (res.status >= 400) {
+                        throw new Error("Bad response");
+                    }
+                    return res.json();
+                }).then(data => {
+                    poster_controller.abort(); 
+                    let posterDB = data.results[0];
+                    var fullPosterUrl;
+                    if (posterDB) {
+                        fullPosterUrl = "http://image.tmdb.org/t/p/w500/"+posterDB.poster_path;
+                    } else {
+                        fullPosterUrl = "https://www.wpbeginner.com/wp-content/uploads/2013/04/wp404error.jpg";
+                    }
+                    posterLinks.push(fullPosterUrl);
+                })
+            })
+
+            posterLinks.map(a => console.log(a));
+            setMoviePosters(posterLinks);
+        }
+    }
 
     useEffect( async () => {
         await fetch(request_path, {
@@ -33,7 +68,9 @@ const MovieCarousel = () => {
             }
             return res.json();
         }).then(data => {
+            controller.abort();
             setMoviesList(data.recordsets[0]);
+            fetchPosterData(data.recordsets[0]);
         }).catch(err => {
             console.log(err)
         })
@@ -55,8 +92,15 @@ const MovieCarousel = () => {
         setActiveId(nextId);
     }
 
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveId(newIndex);
+    }
 
-    if (moviesList ) {
+
+    if (moviesList && moviePosters ) {
+
+        console.log(moviePosters.length);
 
         const movieSlides = moviesList.reduce((acc, cur, id, arr) => {
             if (id%numMovieEachPage === 0) 
@@ -70,10 +114,19 @@ const MovieCarousel = () => {
                               onExited={() => setAnimating(false)}
                               key={index}>
                     <Row>
-                        {items.map(item => {
+                        {items.map((item,id) => {
                             return (
                                 <Col key={item["tconst"]}>
-                                    <p className="small-font">{item["primaryTitle"]}</p>
+
+                                    <Card inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+                                        <CardImg top width="10%" height="200px"
+                                                 src={moviePosters[id + index * numMovieEachPage]}
+                                                 />
+                                        <CardBody>
+                                            <CardLink className="movie-overview-thicker" href = "#">{item["primaryTitle"]}</CardLink>
+                                        </CardBody>
+                                    </Card>
+
                                 </Col>
                             );
 
@@ -85,13 +138,19 @@ const MovieCarousel = () => {
         })
 
         return (
-            <div>
-                <p className = "pl-5 movie-carousel-font">Top movies in 2020</p>
+            <>
+            <style>
+                    {`.idiot-tag {
+                        margin-top : -100px;
+                    }`}
+            </style>
+            <div className="idiot-tag">
+                <p className = "py-4 pl-5 movie-carousel-font">Top movies in 2020</p>
                 <div>
                     <style>
                         {`.custom-tag {
                             max-width: 100%;
-                            height: 200px;
+                            height: 500px;
                             background: black;
                         }`}
                     </style>
@@ -99,6 +158,13 @@ const MovieCarousel = () => {
                     
                     <Carousel activeIndex={activeId} next={next} previous={previous}
                               interval={null}>
+                        <CarouselIndicators items={moviesList.reduce((acc, cur, id, arr) => {
+                                                    if (id%numMovieEachPage === 0) 
+                                                    acc.push(arr.slice(id, id+numMovieEachPage));
+                                                    return acc;
+                                                },[])} 
+                                            activeIndex={activeId} 
+                                            onClickHandler={goToIndex}/>
                         {movieSlides}
                         <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
                         <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
@@ -107,6 +173,7 @@ const MovieCarousel = () => {
                 </div>
                 
             </div>
+            </>
         );
     } else {
         return <></>;
